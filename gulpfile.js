a5f6409bc32d58f7f6cce491822d9bf7df902228
Andrey Belyaev <ianbel263@gmail.com>
2021-07-29 14:21:48 +0300

    десктоп

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icons/*.svg")
+  return gulp.src("source/img/icons/spr_*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
